6c74aa16ce0496fe072f11b9df345137f45583fa
Daniil Anekov <daniilanekov@mail.ru>
2023-05-26 11:15:24 +0300

    Update security filter for swagger.

--- src/main/java/com/UserStorage/config/SecurityConfig.java
+++ src/main/java/com/UserStorage/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -29,7 +30,7 @@ public class SecurityConfig {
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/api3/auth/**","/v2/api-docs/**", "/swagger-ui/**", "/swagger-resources/**", "/v2/api-docs/**")
+                .requestMatchers("/api3/auth/**","/swagger-ui/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
@@ -41,6 +42,10 @@ public class SecurityConfig {
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/v3/api-docs/**");
     }
 
 }
